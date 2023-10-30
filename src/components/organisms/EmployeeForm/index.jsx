2a3e260144415112298c0b56aeb76cc8bedc517e
ElvisGalvez <galvez.elvis@gmail.com>
2023-10-30 15:21:40 +0100

    added first/last names validation

--- src/components/organisms/EmployeeForm/index.jsx
+++ src/components/organisms/EmployeeForm/index.jsx
@@ -21,15 +21,44 @@ import ModalCraft from '../../atoms/ConfirmationModal';
 import ModalContent from '../../molecules/ModalContent';
 import './EmployeeForm.css';
 
+const validateName = (name) => {
+  const forbiddenCharacters = [',', ';', ':', '!', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  return !forbiddenCharacters.some(char => name.includes(char));
+};
+
 const EmployeeForm = ({ title, departmentOptions, handleConfirm }) => {
   const [modalOpen, setModalOpen] = useState(false);
   const dispatch = useDispatch();
   const formData = useSelector(state => state.employee);
   const formError = useSelector(state => state.employee.formError); 
 
+  const [firstNameError, setFirstNameError] = useState(''); 
+  const [lastNameError, setLastNameError] = useState('');  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const { firstName, lastName, birthDate, startDate, street, city, state, zipCode, department } = formData;
+  
+    let isValid = true;
+  
+    if (!validateName(firstName)) {
+      setFirstNameError('First name should not contain forbidden characters or numbers.');
+      isValid = false;
+    } else {
+      setFirstNameError('');
+    }
+  
+    if (!validateName(lastName)) {
+      setLastNameError('Last name should not contain forbidden characters or numbers.');
+      isValid = false;
+    } else {
+      setLastNameError('');
+    }
+  
+    if (!isValid) {
+      return;
+    }
+  
     if (firstName && lastName && birthDate && startDate && street && city && state && zipCode && department) {
       setModalOpen(true);
       dispatch(setFormError(''));  
@@ -37,7 +66,7 @@ const EmployeeForm = ({ title, departmentOptions, handleConfirm }) => {
       dispatch(setFormError('Tous les champs doivent être remplis.'));  
     }
   };
-
+  
   const handleCancel = () => {
     setModalOpen(false);
   };
@@ -76,7 +105,9 @@ const EmployeeForm = ({ title, departmentOptions, handleConfirm }) => {
       <h2 className="form-title">{title}</h2>
       <form id="employee-form" onSubmit={handleSubmit}>
         <TextField label="First Name" id="firstName" value={formData.firstName} onChange={handleChange('firstName')} />
+        {firstNameError && <p className="field-error">{firstNameError}</p>} 
         <TextField label="Last Name" id="lastName" value={formData.lastName} onChange={handleChange('lastName')} />
+        {lastNameError && <p className="field-error">{lastNameError}</p>}  
         <DateField label="Date of Birth" id="birthDate" value={formData.birthDate} onChange={handleChange('birthDate')} />
         <DateField label="Start Date" id="startDate" value={formData.startDate} onChange={handleChange('startDate')} />
         <AddressFieldset street={formData.street} city={formData.city} state={formData.state} zipCode={formData.zipCode} onChange={handleChange} />
